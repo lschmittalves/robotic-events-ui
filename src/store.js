f158993fcb705c35a96d1476faa42376757a5faa
Leonardo Alves <leonardoalves@Macbooks-MacBook-Pro.local>
2019-10-08 22:58:36 -0500

    Start configuring the state control

--- src/store.js
+++ src/store.js
@@ -3,14 +3,38 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+function initialState () {
+  return {
+    userIsLogged: false,
+    currentUserInfo: {},
+    currentEventInfo: {
+      registerTeams: [],
+      robotsPerCategory: [],
+      availableCategories: []
+    },
+    currentTeam: {
+      robots: [],
+      registerRobots: [],
+      persons: [],
+      registeredPersons: []
+    }
   }
+}
+
+export default new Vuex.Store({
+  state: initialState,
+  mutations: {
+    LOGGOUT: state => {
+      // acquire initial state
+      const s = initialState()
+      Object.keys(s).forEach(key => {
+        state[key] = s[key]
+      })
+    },
+    LOGIN: (state, person) => {
+      state.currentUserInfo = person
+      state.userIsLogged = true
+    }
+  },
+  actions: {}
 })
